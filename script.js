62fb2f81583fc032eb8d34d0a91b8dfa81557863
Tiane Zerba <148304153+tianenaomi@users.noreply.github.com>
2024-12-12 13:37:54 +1000

    SOLVED gameboard IIFE

--- script.js
+++ script.js
@@ -1,3 +1,128 @@
+let gameBoard = (function(){
+    let _gameBoard = ["", "", "", "", "", "", "", "", ""];
+    /*DESTRUCTURE THE GAMEBOARD INTO ROWS
+    let [a, b, c] = _gameBoard;
+    let [d, e, f] = _gameBoard;
+    let [g, h, i] = _gameBoard;
+    [a, d, g] = _gameBoard;
+    [b, e, h] = _gameBoard;
+    [c, f, i] = _gameBoard;
+    [a, e, i] = _gameBoard;
+    [c, e, g] = _gameBoard;*/
+    return {
+        chooseCell: function(cell, token){
+            return _gameBoard[cell] = token;
+        },
+        getGameBoard: function() {
+            return _gameBoard;
+        },
+        getTopRow: function() {
+            [a, b, c] = _gameBoard;
+            return [a, b, c];
+        },
+        getMidRow: function() {
+            [d, e, f] = _gameBoard;
+            return [d, e, f];
+        },
+        getBtmRow: function() {
+            [g, h, i] = _gameBoard;
+            return [g, h, i];
+        },
+        getLftCol: function() {
+            [a, d, g] = _gameBoard;
+            return [a, d, g];
+        },
+        getMidCol: function() {
+            [b, e, h] = _gameBoard;
+            return [b, e, h];
+        },
+        getRgtCol: function() {
+            [c, f, i] = _gameBoard;
+            return [c, f, i];
+        },
+        getLftDiag: function() {
+            [a, e, i] = _gameBoard;
+            return [a, e, i];
+        },
+        getRgtDiag: function() {
+            [c, e, g] = _gameBoard;
+            return [c, e, g];
+        },
+    };
+}());
+
+// console.log(gameBoard.getGameBoard()); //returns array of empty strings
+// console.log(gameBoard.chooseCell(4, 'o')); //returns 'o'
+// console.log(gameBoard); //returns updated array
+console.log(gameBoard.chooseCell(0, 'x'));
+console.log(gameBoard.chooseCell(5, 'o'));
+console.log(gameBoard.getTopRow());
+
+let players = (function(){
+    function createPlayer(name, token){
+        return {player: name, token: token}
+    }
+
+    const _playerX = createPlayer("playerX", "X");
+    const _playerO = createPlayer("playerO", "O");
+
+    return {
+        getPlayerX: function(){
+            return _playerX;
+        },
+        getPlayerO: function(){
+            return _playerO;
+        },
+    }
+}());
+
+// console.log(players.getPlayerX());
+// console.log(players.getPlayerO());
+// console.log(players);
+
+let gameFlow = function(){
+    let _whoseTurn = players.getPlayerX();
+    console.log(_whoseTurn);
+    let _gameOver = false;
+
+    function takeTurn(){
+        gameBoard.chooseCell(cell, token);
+        (_whoseTurn === players.getPlayerX()) ? 
+            _whoseTurn === players.getPlayerO() :
+            _whoseTurn === players.getPlayerX();
+    }
+    // 3. gameBoard array
+    //     TOP ROW: 0  1   2
+    //     MID ROW: 3  4   5
+    //     BTM ROW: 6  7   8
+
+    function checkForMatches(array){
+        
+    }
+
+    function checkWin(){
+    // DEFINE checkForWin function
+    // ACCESS gameBoard
+    // IF (top row OR mid row OR btm row OR 
+        gameBoard.getTopRow().forEach(cell) => {
+
+        }
+    //     left col OR mid col OR rgt col OR
+    //     diag OR diag2) tokens match THEN
+        
+    //     SET gameOver to true
+    //     RETURN winning player
+
+    // ELSE IF all tokens have been placed AND no matches THEN
+    //     SET gameOver to true
+    //     RETURN tie
+    // ELSE gameOver remains false
+    // END checkForWin
+    }
+
+}
+
+console.log(gameFlow());
 
 /* ======================================================
                     PSEUDOCODE
